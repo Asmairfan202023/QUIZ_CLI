12419b457f0924f06f0788de520d34185c8c2298
Asmairfan202023 <160836108+Asmairfan202023@users.noreply.github.com>
2024-06-09 13:55:45 +0500

    QUIZ_CLI

---
+++ app.ts
@@ -0,0 +1,63 @@
+#! /usr/bin/env node
+import inquirer from 'inquirer';
+
+type Question = {
+  question: string;
+  choices: string[];
+  answer: string;
+};
+
+const questions: Question[] = [
+  {
+                question: "What is the output of the following code? enum Color {Red,Green, Blue}  console.log(Color.green)",
+                choices: ["Green", "1", "Color_Green", "undefined"],
+                answer: "1"
+            },
+            {
+                question: "______ is return type for when nothing is returned'?",
+                choices: ["void", "unknown", "any", "any[]"],
+                answer: "void"
+            },
+            {
+                question: "Numeric enum first value is defaulted to What",
+                choices: ["0", "1", "10", "5"],
+                answer: "0"
+            },
+            {
+                question: "________ is an interface will have the same propertiesas that interface?",
+                choices: ["duplication", "extending", "idolizing", "improving"],
+                answer: "extending"
+              }
+
+    
+  ];
+
+const askQuestion = async (question: Question) => {
+  const answers = await inquirer.prompt([
+    {
+      type: 'list',
+      name: 'userAnswer',
+      message: question.question,
+      choices: question.choices
+    }
+  ]);
+  return answers.userAnswer === question.answer;
+};
+
+const runQuiz = async () => {
+  console.log("Welcome to the Quiz!");
+  let score = 0;
+  for (const question of questions) {
+    const isCorrect = await askQuestion(question);
+    if (isCorrect) {
+      console.log("Correct!");
+      score++;
+    } else {
+      console.log(`Wrong! The correct answer is ${question.answer}.`);
+    }
+  }
+  console.log(`Quiz finished! Your score is ${score}/${questions.length}`);
+};
+
+runQuiz();
+
